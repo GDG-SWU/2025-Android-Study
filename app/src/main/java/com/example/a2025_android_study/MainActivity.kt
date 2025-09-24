d736c0ae80bfa5b443272bd7770dbb383a3091b9
hyunning <flower4351@gamil.com>
2025-09-25 00:17:49 +0900

    hyunning week1

--- app/src/main/java/com/example/a2025_android_study/MainActivity.kt
+++ app/src/main/java/com/example/a2025_android_study/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.a2025_android_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.a2025_android_study.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    var testCount = 20
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val person = Person("념념",20)
+        binding.hyun = person
+
+    }
+
+    fun myClick(view : View){
+        Log.d("MainActivity", "onClick")
+        testCount++
+
+        val person = Person("념념",testCount)
+        binding.hyun = person
     }
 }
