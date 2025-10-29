344f6a2ea0e9aea1ad922f983dafb93a2b00e43d
hyunning <flower4351@gamil.com>
2025-10-29 22:42:34 +0900

    hyunning-week4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation ("androidx.core:core-ktx:1.12.0") // 둘 중 하나만 남김
+    implementation ("androidx.core:core:1.12.0")    // 아래쪽 블록에만 있던 내용
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
